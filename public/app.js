35577ea909a5256d383b295a8d58ea696d4a481d
OPR_A <opr.a.ruby@gmail.com>
2020-03-10 23:23:16 +0400

    Keep x variable in  localStore

--- public/app.js
+++ public/app.js
@@ -1,10 +1,20 @@
 
 function something()
 {
+	// При каждом обращении к something() создаётся новая переменная x
+	//  Эта переменная доступна только внутри этого метода
 
-	var x = window.localStorage.getItem('aaa');
+	// Получаем значение  по ключу 'aaa' из хеша localStorage и записываем её в переменную x 
+	var x = window.localStorage.getItem('bbb');
 
-	window.localStorage.setItem('aaa', 5);
+	// Увеличиваем переменную x на 1. Без умножения получается добавление 1 к строке x 
+	x = x * 1 + 1; 
+
+	// Отправить значение x в хеш localStorege с ключём 'aaa'  {'aaa' => x}
+	window.localStorage.setItem('bbb', x);
+
+	// Выводим окно с хначением переменной x
 	alert(x);
 
+	// Забываем переменную x
 }
