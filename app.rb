7607bce9c913786034fcee2f69413a87fcd494c5
OPR_A <opr.a.ruby@gmail.com>
2020-03-10 23:50:52 +0400

    Add model Product

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require 'sinatra/activerecord'
 
 set :database, 'sqlite3:pizzashop.db'
 
-class Product < ActiveRecord::Base
-end
+	class Product < ActiveRecord::Base
+	end
 
 get '/' do
 	erb :index
