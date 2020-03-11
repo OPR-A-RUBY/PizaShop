f60514dd264c28c61df207e322fce37775e497b8
OPR_A <opr.a.ruby@gmail.com>
2020-03-11 16:32:22 +0400

    Editing images 2

---
+++ db/migrate/20200311075513_add_pizzas.rb
@@ -0,0 +1,72 @@
+class AddPizzas < ActiveRecord::Migration[6.0]
+  
+  	def change
+  	
+  		Product.create( 
+  			:title => 'Сырная', 
+  			:discription => ' Лучшая питца для завтрака. Очень сытная и любимая многими гурманами.', 
+  			:price => 300, 
+  			:size => 40, 
+  			:is_spicy => false,	
+  			:is_veg => false, 
+  			:is_best_offer => false, 
+  			:path_of_image => '/images/pizza_cheese.jpg' 
+  			)
+
+  		Product.create( 
+  			:title => 'Классическая', 
+  			:discription => ' Если теряетесь какую питцу выбрать, то этот вариант для Вас.', 
+  			:price => 350, 
+  			:size => 40, 
+  			:is_spicy => false,	
+  			:is_veg => false, 
+  			:is_best_offer => false, 
+  			:path_of_image => '/images/pizza_classic.jpg' 
+  			)
+
+  		Product.create( 
+  			:title => 'Гавайская', 
+  			:discription => ' Название говорит само за себя. Экзотические овочи в составе этой питцы Вас удивят новыми вкусами.', 
+  			:price => 300, 
+  			:size => 40, 
+  			:is_spicy => false,	
+  			:is_veg => true, 
+  			:is_best_offer => false, 
+  			:path_of_image => '/images/hawaiian.jpg' 
+  			)
+
+  		Product.create( 
+  			:title => 'Малышка', 
+  			:discription => ' Маленькую питцу делают для маленьких детей.', 
+  			:price => 150, 
+  			:size => 25, 
+  			:is_spicy => false,	
+  			:is_veg => false, 
+  			:is_best_offer => true, 
+  			:path_of_image => '/images/pizza_litle.jpg' 
+  			)
+
+  		Product.create( 
+  			:title => 'Перчик', 
+  			:discription => ' Острая питца для любителей поострее. Халапенью тому подтверждение.', 
+  			:price => 300, 
+  			:size => 40, 
+  			:is_spicy => true,	
+  			:is_veg => false, 
+  			:is_best_offer => false, 
+  			:path_of_image => '/images/pizza_peper.jpg' 
+  			)
+
+  		Product.create( 
+  			:title => 'Пеперони', 
+  			:discription => ' Классическая питца с колбасой Peperoni. Те, кто пробовал, оценили её по достоинстиву.', 
+  			:price => 400, 
+  			:size => 40, 
+  			:is_spicy => true,	
+  			:is_veg => false, 
+  			:is_best_offer => false, 
+  			:path_of_image => '/images/pizza_peperoni.jpg' 
+  			)
+
+  	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_195337) do
+ActiveRecord::Schema.define(version: 2020_03_11_075513) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
