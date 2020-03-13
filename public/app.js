10ffca2acebba88dc4d2fdf805f20a39a6abb94a
OPR_A <opr.a.ruby@gmail.com>
2020-03-13 23:13:25 +0400

    Add cart_get_number_of_item() function

--- public/app.js
+++ public/app.js
@@ -27,4 +27,30 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);	// Считываем из хеша value по КЛЮЧУ
 	x = x * 1 + 1;								// Увеличиваем значение на единицу
 	window.localStorage.setItem(key, x);		// Записываем новое значение в хеш
+
+	alert('Количество питц в Вашей корзине ' + cart_get_number_of_item());
+}
+
+function cart_get_number_of_item()
+{
+	// Команды скрипта работают в консоле браузера, т.е. их можно там оттестить.
+	var cnt = 0;
+
+	for ( i = 0;  i < window.localStorage.length; i++)
+	{
+		// Получаем ключ текущего элемента в хеше (записываем его в key
+		var key = window.localStorage.key(i);
+
+		// Получаем значение из хеша по ключу (записываем это значение в value)
+		var value = window.localStorage.getItem(key);
+
+		// Если ключ key начинается с 'product_' или дословно: если позиция (index Of) с которой
+		// начинается строка 'product_' - это нулевая позикия в текущем ключе key, то ...
+		if(key.indexOf('product_') == 0)
+		{
+			//cnt++;  // ... увеличиваем cnt на 1  (плюс один вид товара)
+			cnt = cnt + value*1;
+		} 		
+	}
+	return cnt;
 }
