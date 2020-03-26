1d65590ae92bc97f8a08c305ff30c75911cd0d32
OPR_A <opr.a.ruby@gmail.com>
2020-03-26 08:31:46 +0400

    #

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+# rem
 set :database, 'sqlite3:pizzashop.db'
 
 class Product < ActiveRecord::Base
@@ -11,46 +12,46 @@ end
 # .string .text .integer .float .decimal .datetime .timestamp .time .date .binary .boolean
 
 get '/' do
-	@product = Product.all 
-	erb :index
+    @product = Product.all 
+    erb :index
 end
 
 get '/about' do
-	erb :about
+    erb :about
 end
 
 post '/cart' do
-	@order = params[:order]
-	@product = Product.all
-	puts @order
-	@hh = {}
-	n = ""
-	key = ""
-	value = ""
-	@order.each_char do |item|
-		if item == ","
-			# Делим переменную на key и value
-			puts n
-			i = 0
-			s = n.size
-			n.each_char do |item_i|
-				i = i + 1
-				if item_i == "="
-					key = n[8,i-1-8]
-					value = n[i,s]
-					i = 0
-					puts key
-					puts value
-				end		
-			end
-			# Пишем в хеш данные по товару
-			@hh[key] = value.to_i
-			# Обнуляем переменные для нового товара
-			n = ""
-		else
-			n = n + item
-		end				
-	end
-	puts @hh
-	erb :cart
+    @order = params[:order]
+    @product = Product.all
+    puts @order
+    @hh = {}
+    n = ""
+    key = ""
+    value = ""
+    @order.each_char do |item|
+        if item == ","
+            # Делим переменную на key и value
+            puts n
+            i = 0
+            s = n.size
+            n.each_char do |item_i|
+                i = i + 1
+                if item_i == "="
+                    key = n[8,i-1-8]
+                    value = n[i,s]
+                    i = 0
+                    puts key
+                    puts value
+                end     
+            end
+            # Пишем в хеш данные по товару
+            @hh[key] = value.to_i
+            # Обнуляем переменные для нового товара
+            n = ""
+        else
+            n = n + item
+        end             
+    end
+    puts @hh
+    erb :cart
 end
